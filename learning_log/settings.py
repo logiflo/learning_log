aa66ebafa68e3dee4b535b2256c278acd1c7f0e1
logiflo <logiflo5@gmail.com>
2020-08-07 15:04:51 +0100

    Minor fix

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -67,9 +67,9 @@ if os.getcwd() == '/app':
     #Static asset configuration
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     STATIC_ROOT = 'staticfiles'
-    STATICFILES_DIRS = (
+    STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static'),
-    )
+    ]
 
 # Application definition
 
